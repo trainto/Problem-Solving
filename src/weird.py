f118848b786911aa076baef3f44a6201def8eb50
trainto <trainto@gmail.com>
2016-06-10 17:44:43 +0900

    Progress on WEIRD

--- src/weird.py
+++ src/weird.py
@@ -1,15 +1,43 @@
-from functools import reduce
+import time
 
-numtc = int(input())
+class Node:
+    def __init__(self, val, depth, isLeft):
+        self.val = val
+        self.depth = depth
+        self.isLeft = isLeft
+    def addLeft(self, node):
+        self.left = node
+    def addRight(self, node):
+        self.right = node
 
 def isWeird(divisors, num):
-    # BackTracking
+    root = Node(0, 0, False)
+    stack = []
+    stack.append(root)
+    while len(stack) != 0:
+        cur = stack.pop()
+        if (cur.val == num):
+            return False
+        elif cur.val > num:
+            if cur.isLeft:
+                stack.pop()
+            continue
+        else:
+            if cur.depth + 1 > len(divisors):
+                continue
+            cur.addLeft(Node(cur.val + divisors[cur.depth], cur.depth + 1, True))
+            cur.addRight(Node(cur.val, cur.depth + 1, False))
+            stack.append(cur.right)
+            stack.append(cur.left)
+    return True
 
-for i in range(numtc):
+for i in range(int(input())):
     num = int(input())
+    start = time.time()
     divisors = list(filter(lambda x: num % x == 0, range(1, num)))
     if sum(divisors) > num: # first condition
         if isWeird(divisors, num):
             print("weird")
             continue
     print("not weird")
+    print(time.time() - start)

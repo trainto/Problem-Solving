7f6032a19e6fcec313d7e7194cca276474dc6196
trainto <trainto@gmail.com>
2016-06-10 09:28:31 +0900

    Solving BRACKETS2

---
+++ src/brackets2.py
@@ -0,0 +1,27 @@
+def isOpen(ch):
+    if (ch == '(' or ch == '{' or ch == '['):
+        return True;
+    return False;
+
+def isMatched(pair):
+    if (pair == '()' or pair == '{}' or pair == '[]'):
+        return True;
+    return False;
+
+for i in range(int(input())):
+    data = input()
+    stack = []
+    isFailed = False;
+    for j in range(len(data)):
+        if isOpen(data[j]):
+            stack.append(data[j])
+        else:
+            if len(stack) != 0 and isMatched(stack.pop() + data[j]):
+                continue
+            else:
+                isFailed = True
+                break
+    if len(stack) == 0 and not(isFailed):
+        print('YES')
+    else:
+        print('NO')

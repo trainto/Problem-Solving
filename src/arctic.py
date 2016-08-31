d94ad28afaad57da9236394e5f68093264e047a6
trainto <trainto@gmail.com>
2016-08-31 14:25:10 +0900

    ARCTIC solved

--- src/arctic.py
+++ src/arctic.py
@@ -1,35 +1,44 @@
 import sys
-import math
-
-def getDistance(a, b):
-    return math.sqrt(pow(a[0] - b[0], 2) + pow(a[1] - b[1], 2))
 
 rl = lambda: sys.stdin.readline()
 for _ in range(int(rl())):
     stations = []
     for _ in range(int(rl())):
         stations.append(list(map(float, rl().split())))
+    # For test case
+    # stations = [[1.0, 1.0], [30.91, 8.0], [4.0, 7.64], [21.12, 6.0], [11.39, 3.0], [5.31, 11.0]]
+    # for _ in range(100):
+        # x = random.random() * 100
+        # y = random.random() * 100
+        # stations.append([x, y])
+
     visited = [False] * len(stations)
-    memo = [[-1 for col in range(len(stations))] for row in range(len(stations))]
+    memo = [[[-1, -1] for _ in range(len(stations))] for _ in range(len(stations))]
+    # Cal all distances and sort memo[][] = [to what station, distance]
+    for row in range(len(stations) - 1):
+        for col in range(row + 1, len(stations)):
+            distance = (stations[row][0] - stations[col][0]) ** 2 + (stations[row][1] - stations[col][1]) ** 2
+            memo[row][col][0] = col
+            memo[row][col][1] = distance
+            memo[col][row][0] = row
+            memo[col][row][1] = distance
+    for row in range(len(stations)):
+        memo[row].sort(key=lambda x: x[1])
+        memo[row].pop(0) # Remove [-1, -1] which is initial value for distance to themselves
 
     maxFrequency = 0
     visited[0] = True
     while False in visited:
-        distance = 0xffffffff
+        minDistance = 0xffffffff
         selectedStation = 0
         for curStation in (list(filter(lambda x: visited[x] == True, range(len(visited))))):
-            for candidate in (list(filter(lambda x: visited[x] == False, range(len(visited))))):
-                if memo[curStation][candidate] != -1:
-                    candidateDistance = memo[curStation][candidate]
-                else:
-                    candidateDistance = getDistance(stations[curStation], stations[candidate])
-                    memo[curStation][candidate] = candidateDistance
-                    memo[candidate][curStation] = candidateDistance
-                if candidateDistance < distance:
-                    distance = candidateDistance
-                    selectedStation = candidate
-                    visited[selectedStation] = True
-            if distance > maxFrequency:
-                maxFrequency = distance
+            if visited[memo[curStation][0][0]]:
+                memo[curStation].pop(0)
+            if minDistance > memo[curStation][0][1]:
+                minDistance = memo[curStation][0][1]
+                selectedStation = memo[curStation][0][0]
+        visited[int(selectedStation)] = True
+        if maxFrequency < minDistance:
+            maxFrequency = minDistance
 
-    print("%0.2f" % maxFrequency)
+    print("{:.2f}".format(round(maxFrequency ** 0.5, 2)))

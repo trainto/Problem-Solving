261dd7240f8a732b061dc01c155b97a5ca97a401
trainto <trainto@gmail.com>
2018-04-24 12:06:53 +0900

    Solved TSP1

---
+++ src/tsp1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef enum { false, true } bool;
+#define MIN(x, y) (((x) <(y)) ? (x) : (y))
+
+int cities;
+double dist[8][8];
+bool visited[8];
+
+double tsp(double currentDist, int prevCity, int visitedN)
+{
+    if (visitedN == cities) {
+        return currentDist;
+    }
+
+    double ret = INFINITY;
+    for (int i = 0; i < cities; i++) {
+        if (visited[i]) {
+            continue;
+        }
+        visited[i] = true;
+        visitedN++;
+        double calDist =
+            tsp(prevCity == -1 ? currentDist : currentDist + dist[prevCity][i],i, visitedN);
+        ret = MIN(ret, calDist);
+
+        visited[i] = false;
+        visitedN--;
+    }
+
+    return ret;
+}
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &cities);
+
+        for (int j = 0; j < cities; j++) {
+            for (int k = 0; k < cities; k++) {
+                scanf("%lf", &dist[j][k]);
+            }
+        }
+
+        printf("%.10f\n", tsp(0.0, -1, 0));
+        for (int j = 0; j < cities; j++) {
+            visited[j] = false;
+        }
+    }
+}
